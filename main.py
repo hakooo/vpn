52da4acbecba11ee7083bc071adaac56c8c14ab2
hako0o00 <la_amini@esi.dz>
2023-06-20 13:34:41 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+
+
+class BoxLayoutExample(BoxLayout):
+	pass
+
+class MainWidget(Widget):
+	pass
+
+class VpnApp(App):
+	pass
+
+
+VpnApp().run()
